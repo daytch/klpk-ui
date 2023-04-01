ae2e7c4316d2910dbff40beb6e08fa0235c25dd7
Luthfi Tri Atmaja <luthfi.personal@gmail.com>
2023-04-01 15:17:15 +0700

    fix: avatar sizes and clickable profile card

--- src/components/organisms/cards/ProfileUserCard.tsx
+++ src/components/organisms/cards/ProfileUserCard.tsx
@@ -2,8 +2,10 @@ import React, { useMemo } from 'react'
 import Image from 'next/image'
 import { joinClass } from '@/utils/common'
 import { ProfilePhotoDataModel } from '@/interfaces/profile'
+import Link from '@/components/atoms/Link'
 
 type ProfileUserCardProps = {
+  id: string,
   profilePhoto?: ProfilePhotoDataModel[]
   name: string
   isActive?: boolean
@@ -12,6 +14,7 @@ type ProfileUserCardProps = {
 }
 
 export default function ProfileUserCard({
+  id,
   profilePhoto,
   name,
   isActive,
@@ -33,16 +36,18 @@ export default function ProfileUserCard({
   }
   return (
     <div className="flex items-center justify-between space-x-4">
-      <div className="flex items-center space-x-6 text-kplkWhite">
-        <Image
-          src={userAvatarImage}
-          alt=""
-          width={55}
-          height={55}
-          className="rounded-full"
-        />
-        <p className="text-sm font-gotham font-medium leading-3">{name}</p>
-      </div>
+      <Link to={`/profile/penulis/${id}`} className='block'>
+        <div className="flex items-center space-x-6 text-kplkWhite">
+          <Image
+            src={userAvatarImage}
+            alt={name}
+            width={55}
+            height={55}
+            className="w-[55px] h-[55px] rounded-full object-cover"
+          />
+          <p className="text-sm font-gotham font-medium leading-3">{name}</p>
+        </div>
+      </Link>
       <button
         onClick={handleClickButton}
         type="button"
@@ -53,6 +58,6 @@ export default function ProfileUserCard({
       >
         {isActive ? 'Following' : 'Follow'}
       </button>
-    </div>
+    </div >
   )
 }
