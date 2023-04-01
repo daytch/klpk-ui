5adeb03fbf9a9dea0e40347881ee74eb8b489884
Luthfi Tri Atmaja <luthfi.personal@gmail.com>
2023-04-01 14:56:08 +0700

    adjust rating decimal places

--- src/components/molecules/ProductCard/index.tsx
+++ src/components/molecules/ProductCard/index.tsx
@@ -27,7 +27,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
           <div className="bg-[#f9f7efe6] inline-flex items-center space-x-[2px] p-[5px] rounded-lg absolute top-2 left-2 z-[2]">
             <IconStar />
             <span className="font-gotham text-sm font-light leading-3 text-gold-100">
-              {book?.rating ?? 0}
+              {(book?.rating ?? 0).toFixed(1)}
             </span>
           </div>
         )}
