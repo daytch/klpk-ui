edea8a210b36458cd51a982ee8671ed137fb051c
Tatak Galih <tatakgalih@yahoo.com>
2023-07-01 13:29:45 +0700

    change termsn and privacy

--- src/pages/privacy_policy/index.tsx
+++ src/pages/privacy_policy/index.tsx
@@ -11,195 +11,113 @@ const PrivacyPolicyPage = () => {
         <div className="container space-y-6 py-10">
           <div className="leading-[180%]">
             <h1 className="text-4xl font-bold text-gold-200 mb-3">
-              Privacy Policy
+              Privacy Policy <br />
+              Effective Date: 30 June, 2023
             </h1>
             <p className="text-base text-kplkWhite mb-2">
-              KLPK Indonesia built the KLPK App as a Free app. This SERVICE is
-              provided by KLPK App Indonesia at no cost and is intended for use
-              as is.
+              At Komunitas Literasi Patrick Kellan ("KLPK" "we" or "us"), we are
+              committed to protecting your privacy and keeping your information
+              safe. This Privacy Policy explains how we collect, use, disclose
+              and keep information that can identify you ("Personal
+              Information") when you create a KLPK account and use
+              komunitaspatrickkellan.com (the "Site"), or our mobile
+              applications, to post content to the KLPK, including stories you
+              create.
             </p>
             <p className="text-base text-kplkWhite mb-2">
-              This page is used to inform visitors regarding our policies with
-              the collection, use, and disclosure of Personal Information if
-              anyone decided to use our Service.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              If you choose to use our Service, then you agree to the collection
-              and use of information in relation to this policy. The Personal
-              Information that we collect is used for providing and improving
-              the Service. we will not use or share your information with anyone
-              except as described in this Privacy Policy.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              The terms used in this Privacy Policy have the same meanings as in
-              our Terms and Conditions, which is accessible at KLPK App unless
-              otherwise defined in this Privacy Policy.
+              By using KLPK, you agree to the collection and use of your
+              personal information as described in this Privacy Policy. This is
+              important; we hope you will take time to read it carefully.
             </p>
           </div>
+
           <div className="leading-[180%]">
             <h2 className="text-base font-bold text-gold-200 mb-3">
-              Google Login Policy
+              Information We Collect
             </h2>
-            <p className="text-base text-kplkWhite mb-2">
-              By installing the KLPK App and logging in by Google, you agree to
-              the terms and conditions of sharing data between Google Credential
-              and KLPK App. The data taken is only the Account ID, Given Name,
-              Family Name, Google Photo Profile, and email address. The data is
-              used for the benefit of your profile in the KLPK App and will not
-              be distributed outside the application. The information will be
-              sent to our old and new server (https://ketix.me or
-              https://iamhoster.com).
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">
-              Information Collection and Use
-            </h2>
-            <p className="text-base text-kplkWhite mb-2">
-              For a better experience, while using our Service, we may require
-              you to provide us with certain personally identifiable
-              information. The information that we request will be retained by
-              us and used as described in this privacy policy.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              The app does use third party services that may collect information
-              used to identify you.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              Link to privacy policy of third party service providers used by
-              the app
-            </p>
-            <ul className=" list-disc pl-8">
-              <li>
-                <a
-                  className="text-[#007bff] underline hover:no-underline"
-                  href="https://www.google.com/policies/privacy/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Google Play Services
-                </a>
+            <ul className="list-disc space-y-2 pl-4">
+              <li className="text-base text-kplkWhite">
+                Personal Information <br />
+                We may collect the following types of personal information from
+                you: Name, Email Address, User-generated content (such as
+                stories) that you submit through the KLPK, Usage data and
+                analytics (including device information, IP address, browser
+                type, and access times)
+              </li>
+              <li className="text-base text-kplkWhite">
+                Non-Personal Information
+                <br />
+                We may collect non-personal information that does not directly
+                identify you, such as aggregated data or statistical information
+                about app usage.
               </li>
             </ul>
           </div>
           <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">Log Data</h2>
-            <p className="text-base text-kplkWhite mb-2">
-              we want to inform you that whenever you use our Service, in a case
-              of an error in the app we collect data and information (through
-              third party products) on your phone called Log Data. This Log Data
-              may include information such as your device Internet Protocol
-              (“IP”) address, device name, operating system version, the
-              configuration of the app when utilizing our Service, the time and
-              date of your use of the Service, and other statistics.
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">Cookies</h2>
-            <p className="text-base text-kplkWhite mb-2">
-              Cookies are files with a small amount of data that are commonly
-              used as anonymous unique identifiers. These are sent to your
-              browser from the websites that you visit and are stored on your
-              device's internal memory.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              This Service does not use these “cookies” explicitly. However, the
-              app may use third party code and libraries that use “cookies” to
-              collect information and improve their services. You have the
-              option to either accept or refuse these cookies and know when a
-              cookie is being sent to your device. If you choose to refuse our
-              cookies, you may not be able to use some portions of this Service.
-            </p>
+            <h2 className="text-base font-bold text-gold-200 mb-3">
+              Use of Information
+            </h2>
+            <ul className="list-disc space-y-2 pl-4">
+              <li className="text-base text-kplkWhite">
+                Personal Information <br />
+                We may user your personal information for the following
+                purposes:
+                <br />
+                To provide and improve the KLPK functionality and user
+                experience
+                <br />
+                To communicate with you, respond to your inquiries, and provide
+                customer support
+                <br />
+                To send you updates, newsletters, and promotional materials
+                relates to the KLPK
+                <br />
+                To enforce our terms of service and prevent misuse of the KLPK
+              </li>
+              <li className="text-base text-kplkWhite">
+                Non-Personal Information
+                <br />
+                We may use non-personal information for purposes such as data
+                analysis, improving KLPK, and enhancing our services
+              </li>
+            </ul>
           </div>
           <div className="leading-[180%]">
             <h2 className="text-base font-bold text-gold-200 mb-3">
-              Service Providers
+              Your Choices and Rights
             </h2>
             <p className="text-base text-kplkWhite mb-2">
-              we may employ third-party companies and individuals due to the
-              following reasons:
+              You have the right to:
             </p>
             <ul className="list-disc space-y-2 pl-4">
               <li className="text-base text-kplkWhite">
-                To facilitate our Service;
+                Access, correct, or update your personal information
               </li>
               <li className="text-base text-kplkWhite">
-                To provide the Service on our behalf;
+                Request the deletion of your personal information
               </li>
               <li className="text-base text-kplkWhite">
-                To perform Service-related services; or
+                Opt-out of receiving marketing communications
               </li>
               <li className="text-base text-kplkWhite">
-                To assist us in analyzing how our Service is used.
+                Object to the processing of your personal information
               </li>
             </ul>
-            <p className="text-base text-kplkWhite mb-2">
-              we want to inform users of this Service that these third parties
-              have access to your Personal Information. The reason is to perform
-              the tasks assigned to them on our behalf. However, they are
-              obligated not to disclose or use the information for any other
-              purpose.
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">Security</h2>
-            <p className="text-base text-kplkWhite mb-2">
-              we value your trust in providing us your Personal Information,
-              thus we are striving to use commercially acceptable means of
-              protecting it. But remember that no method of transmission over
-              the internet, or method of electronic storage is 100% secure and
-              reliable, and we cannot guarantee its absolute security.
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">
-              Links to Other Sites
-            </h2>
-            <p className="text-base text-kplkWhite mb-2">
-              This Service may contain links to other sites. If you click on a
-              third-party link, you will be directed to that site. Note that
-              these external sites are not operated by us. Therefore, we
-              strongly advise you to review the Privacy Policy of these
-              websites. we have no control over and assume no responsibility for
-              the content, privacy policies, or practices of any third-party
-              sites or services.
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">
-              Children’s Privacy
-            </h2>
-            <p className="text-base text-kplkWhite mb-2">
-              These Services do not address anyone under the age of 13. we do
-              not knowingly collect personally identifiable information from
-              children under 13. In the case we discover that a child under 13
-              has provided us with personal information, we immediately delete
-              this from our servers. If you are a parent or guardian and you are
-              aware that your child has provided us with personal information,
-              please contact us so that we will be able to do necessary actions.
-            </p>
-          </div>
-          <div className="leading-[180%]">
-            <h2 className="text-base font-bold text-gold-200 mb-3">
-              Changes to This Privacy Policy
-            </h2>
-            <p className="text-base text-kplkWhite mb-2">
-              we may update our Privacy Policy from time to time. Thus, you are
-              advised to review this page periodically for any changes. we will
-              notify you of any changes by posting the new Privacy Policy on
-              this page.
-            </p>
-            <p className="text-base text-kplkWhite mb-2">
-              This policy is effective as of 2020-04-27
-            </p>
           </div>
           <div className="leading-[180%]">
             <h2 className="text-base font-bold text-gold-200 mb-3">
               Contact Us
             </h2>
             <p className="text-base text-kplkWhite mb-2">
-              If you have any questions or suggestions about our Privacy Policy,
-              do not hesitate to contact us at dev.KLPKapp@gmail.com.
+              If you have any questions, concerns, or requests regarding this
+              Privacy Policy, please contact us at: Komunitas Literasi Patrick
+              Kellan{' '}
+              <a href="mailto:redaksiklpk@gmail.com"> redaksiklpk@gmail.com</a>
+            </p>
+            <p className="text-base text-kplkWhite mb-2">
+              Jl H. Agus Salim Kaliawi,
+              <br /> Tanjung Karang Pusat. <br />
+              Bandar Lampung 35115
             </p>
           </div>
         </div>
