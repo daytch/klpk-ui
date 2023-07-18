4fdd8c8a6ebf44157d7f73b0d3d891e49ab4f6b3
Tatak Galih <tatakgalih@yahoo.com>
2023-07-18 19:08:07 +0700

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,37 @@ import '../styles/globals.css'
 import { ToastProvider } from '@/hooks/useToast'
 import { GOOGLE_AUTH_CLIENT_ID } from '@/utils/constants'
 import { queryClient } from '@/utils/react-query'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider attribute="class">
-          <ToastProvider>
-            <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID!}>
-              <Component {...pageProps} />
-            </GoogleOAuthProvider>
-          </ToastProvider>
-        </ThemeProvider>
-      </Hydrate>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SPG5TM1NHZ"
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-SPG5TM1NHZ');
+        `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider attribute="class">
+            <ToastProvider>
+              <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID!}>
+                <Component {...pageProps} />
+              </GoogleOAuthProvider>
+            </ToastProvider>
+          </ThemeProvider>
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   )
 }
