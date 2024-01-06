b6d4f1c22936d5a506652a2506af9cc0296eb52f
Tatak Galih <tatakgalih@yahoo.com>
2024-01-06 12:03:44 +0700

    fixing middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,26 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// const PUBLIC_FILE = /\.(.*)$/
+const PUBLIC_FILE = /\.(.*)$/
 // const MOBILE_REGEX = /android|iphone|kindle|ipad/i
 // const ALLOW_MOBILE = ['/auth/verify', '/auth/recover-password']
 
+const ALLOW_MOBILE: string[] = []
+
 export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(new URL('/download-app', request.url))
-  // const { pathname } = request.nextUrl
+  const { pathname } = request.nextUrl
   // const userAgent = request.headers.get('user-agent')
-  // if (
-  //   pathname.startsWith('/_next') ||
-  //   pathname.startsWith('/api') ||
-  //   pathname.startsWith('/static') ||
-  //   PUBLIC_FILE.test(pathname) ||
-  //   ALLOW_MOBILE.includes(pathname)
-  // )
-  //   return NextResponse.next()
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/static') ||
+    PUBLIC_FILE.test(pathname) ||
+    ALLOW_MOBILE.includes(pathname)
+  )
+    return NextResponse.next()
+
+  return NextResponse.rewrite(new URL('/download-app', request.url))
+
   // return MOBILE_REGEX.test(userAgent || '')
   //   ? NextResponse.rewrite(new URL('/download-app', request.url))
   //   : NextResponse.next()
